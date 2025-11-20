7afc01674d6aa786a596aabecd27548aaa3d38be
Boyoung Lee <osoon9295@gmail.com>
2025-11-20 15:37:45 +0900

    테스트

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,49 @@
-// src/app/page.tsx
+// app/page.tsx
+
 "use client";
 
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="w-80">
-        <button
-          onClick={() => {
-            throw new Error("에러 버튼 클릭 sentry 테스트");
-          }}
-        >
-          에러 발생시키
-        </button>
-      </div>
+    <div>
+      <button
+        onClick={async () => {
+          await fetch(
+            "https://discord.com/api/webhooks/1440953460383154318/stCN3c08smnextS_GnzrSXfCawWO1Jrc0VIYlMJj4nF_2r2UPrd40dHDRnt0BqioEpyA",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+              },
+              body: JSON.stringify({
+                content: "Discord 알림 테스트",
+                username: "Alert Bot",
+                embeds: [
+                  {
+                    title: "알림 제목",
+                    description: "알림 설명",
+                    fields: [
+                      {
+                        name: "알림 필드 이름",
+                        value: "알림 필드 값",
+                        inline: true,
+                      },
+                      {
+                        name: "알림 필드 이름",
+                        value: "알림 필드 값",
+                        inline: true,
+                      },
+                    ],
+                    color: 255,
+                  },
+                ],
+              }),
+            }
+          );
+        }}
+        className="rounded-md bg-blue-500 p-2 text-white"
+      >
+        discord로 알림보내기
+      </button>
     </div>
   );
 }
