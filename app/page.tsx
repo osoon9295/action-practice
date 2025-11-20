8b36feda049d547ca9f4f35c9d910939f14875c3
Boyoung Lee <osoon9295@gmail.com>
2025-11-20 15:48:42 +0900

    테스트

--- app/page.tsx
+++ app/page.tsx
@@ -1,44 +1,14 @@
-// app/page.tsx
+// src/app/page.tsx
 
 "use client";
+import * as Sentry from "@sentry/nextjs";
 
 export default function Home() {
   return (
     <div>
       <button
         onClick={async () => {
-          await fetch(
-            "https://discord.com/api/webhooks/1440953460383154318/stCN3c08smnextS_GnzrSXfCawWO1Jrc0VIYlMJj4nF_2r2UPrd40dHDRnt0BqioEpyA",
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-              },
-              body: JSON.stringify({
-                content: "Discord 알림 테스트",
-                username: "Alert Bot",
-                embeds: [
-                  {
-                    title: "알림 제목",
-                    description: "알림 설명",
-                    fields: [
-                      {
-                        name: "알림 필드 이름",
-                        value: "알림 필드 값",
-                        inline: true,
-                      },
-                      {
-                        name: "알림 필드 이름",
-                        value: "알림 필드 값",
-                        inline: true,
-                      },
-                    ],
-                    color: 255,
-                  },
-                ],
-              }),
-            }
-          );
+          Sentry.captureException(new Error("Test Error"));
         }}
         className="rounded-md bg-blue-500 p-2 text-white"
       >
